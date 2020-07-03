ae07357b0460a97b23b8176ec8dab4df9a2bf556
Jakub Gorczynski <jakub.gorczynski00@gmail.com>
2020-07-03 14:09:58 +0200

    regex backreferences tasks

---
+++ regex/backreferences/branch-reset-groups.php
@@ -0,0 +1,14 @@
+<?php
+
+$Regex_Pattern = '/^\d\d(?|(-)|(:)|(---)|(\.))\d\d\1\d\d\1\d\d$/';
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+if(preg_match($Regex_Pattern, $Test_String, $output_array)){
+    print ("true");
+} else {
+    print ("false");
+}
+
+fclose($handle);
+?>
